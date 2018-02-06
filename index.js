07a8d8a44e18bbb59d3120dc7f320e14981a8035
geeuho <geeuho@gmail.com>
2018-02-05 16:30:29 -0800

    final/

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 var Word = require('./word.js')
+var inquirer = require('inquirer');
+
+wordChoice = ['javascript','html','css','node',
+'jquery','firebase', 'ajax', 'mysql'];
+
+var randomWord = wordChoice[Math.floor(Math.random()* wordChoice.length)];
+var guesses = [];
+var guessesLeft = 10;
+
+var userguess = function(){
+
+    console.log("Welcome to Programmer's Hangman!");
+    console.log("Please guess a programming language");
+        var newWord = new Word(randomWord);
+        inquirer.prompt([
+            {
+                type: "input",
+                message: "Please guess a letter",
+                name: "letterGuess"
+            }
+        ]).then (function(character){
+            var letterGuess = character.letterGuess;
+            var guessedStatus = false;
+            for (var i = 0; i < guesses.length; i++){
+                if(letterGuess === guesses[i]){
+                    guessedStatus = true;
+                }
+            } if (guessedStatus === false){
+                guesses.push(letterGuess);
+                var foundWord = newWord.letterCheck(letterGuess)
+            
+                if (foundWord === 0){
+                    console.log("Incorrect Guess!");
+                    console.log(newWord.showWord());
+                    guessesLeft --;
+                } else {
+                    newWord.wordCheck();
+                    newWord.showWord();
+                    console.log("Correct Guess!");
+                    console.log(newWord.showWord())
+                }
+            }
+            if (guessLeft> 0 && newWord.wordFound === false){
+                guessPrompt();
+            } else if (new Word.wordFound === true){
+                console.log("You won! Nice!");
+            } else 
+                console.log("You got no guesses left.");
+        });
+};
+userguess();
--- word.js
+++ word.js
@@ -1,12 +1,41 @@
 var Letter = require ('./letter.js')
 
 var Word = function(word){
+    this.array = [];
+    this.correctarray = [];
     this.word = word
+    this.complete = false;
     Word.prototype.split = function(word){
-        var array = this.word.split('');
-        
+        for (i=0, i < this.word.length, i++){
+            var newLetter = new Letter(this.word[i]);
+            this.letters.push(newLetter);
+        } 
     }
-    this.currentword = function (word){
-        this.word     
+
+    Word.prototype.completeWord = function(){
+        if (this.array.every(function(continue){
+            return continue.appear ===true;
+        })) {
+            this.complete = true;
+            return true;
+        }
+    
     }
-}
+
+    Word.prototype.letterCheck = function (guessedlttr){
+        this.letters.forEach(function(lttr){
+            if(lttr.character === guessedlttr){
+                lttr.appear = true
+            }
+        })
+    }
+    
+    Word.prototype.showWord = function(){
+        var space = '';
+        this.array.forEach(function(display){
+            var checkDisplay = display.check(display);
+            var thisDisplay = display.revealCharacter();
+            space += thisDisplay;
+        })
+    }
+    
